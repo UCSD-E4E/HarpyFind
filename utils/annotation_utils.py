b871b4c4f184db8e52e12e0f1188b588ec467c7b
Matt Epperson <m.p.epperson@gmail.com>
2017-02-22 10:16:37 -0800

    ANNOTATIONS: Rough draft but needs to be tested

--- utils/annotation_utils.py
+++ utils/annotation_utils.py
@@ -1,85 +1,132 @@
 #!/usr/bin/python
-import lxml.etree
-import lxml.builder
+from lxml import etree
+from osgeo import gdal
 
+import numpy as np
 import csv
 
-def generate_pascal_annotation():
+import gdal_utils
 
-    E = lxml.builder.ElementMaker()
-    ANNOTATION = E.annotation
-    FOLDER = E.folder
-    FILENAME = E.filename
-    SOURCE = E.source
-    DATABASE = E.database
-    IMAGE = E.image
-    FLICKRID = E.flickrid
-    OWNER = E.owner
-    NAME = E.name
-    SIZE = E.size
-    WIDTH = E.width
-    HEIGHT = E.height
-    DEPTH = E.depth
-    SEGMENTED = E.segmented
-    OBJECT = E.object
-    POSE = E.pose
-    TRUNCATED = E.truncated
-    DIFFICULT = E.difficult
-    BNDBOX = E.bndbox
-    XMIN = E.xmin
-    YMIN = E.ymin
-    XMAX = E.xmax
-    YMAX = E.ymax
-    LAT1 = E.lat1
-    LON1 = E.lon1
-    LAT2 = E.lat2
-    LON2 = E.lon2
-    USER = E.user
-    i = 0
+def add_object(doc, xmin, ymin, xmax, ymax):
+    # Create Object Tree
+    obj = etree.SubElement(doc, "object")
 
-    with open("../data/annotations/test.csv","rb") as test_in:
-        reader = csv.DictReader(test_in)
-        for row in reader:
-            i = i + 1
-            new_xml_doc = ANNOTATION(
-                            FOLDER('INSERT FOLDER NAME'),
-                            FILENAME('INSERT FILENAME'),
-                            SOURCE(
-                                DATABASE('IS THIS NEEDED?'),
-                                ANNOTATION('IS THIS NEEDED?'),
-                                IMAGE('IS THIS NEEDED?'),
-                                FLICKRID('IS THIS NEEDED?')
-                            ),
-                            OWNER(
-                                FLICKRID('IS THIS NEEDED?'),
-                                NAME('IS THIS NEEDED?')
-                            ),
-                            SIZE(
-                                WIDTH('INSERT WIDTH'),
-                                HEIGHT('INSERT HEIGHT'),
-                                DEPTH('INSERT DEPTH') # RGB
-                            ),
-                            SEGMENTED('IS THIS NEEDED?'),
-                            OBJECT(
-                                NAME('INSERT NAME'),
-                                POSE('IS THIS NEEDED?'),
-                                TRUNCATED('IS THIS NEEDED?'),
-                                DIFFICULT('IS THIS NEEDED?'),
-                                BNDBOX(
-                                    XMIN('INSERT XMIN'),
-                                    YMIN('INSERT YMIN'),
-                                    XMAX('INSERT XMAX'),
-                                    YMAX('INSERT YMAX')
-                                ),
-                                LAT1(row['lat1']),
-                                LON1(row['lon1']),
-                                LAT2(row['lat2']),
-                                LON2(row['lon2']),
-                                USER(row['user'])
-                            )
-                        )
-            doc = lxml.etree.tostring(new_xml_doc, pretty_print=True)
-            with open("../data/annotations/test" + str(i) + ".xml", "w") as outfile:
-                outfile.write(doc)
+    name = etree.SubElement(obj, "name")
+    name.text = "Palm Tree"
 
-generate_pascal_annotation()
+    pose = etree.SubElement(obj, "name")
+    pose.text = "Unspecified"
+
+    truncated = etree.SubElement(obj, "truncated")
+    truncated.text = "0"
+
+    difficult = etree.SubElement(obj, "difficult")
+    difficult.text = "0"
+
+    bndbox = etree.SubElement(obj, "bndbox")
+
+    bb_xmin = etree.SubElement(bndbox, "xmin")
+    bb_xmin.text = str(xmin)
+
+    bb_ymin = etree.SubElement(bndbox, "ymin")
+    bb_ymin.text = str(ymin)
+
+    bb_xmax = etree.SubElement(bndbox, "xmax")
+    bb_xmax.text = str(xmax)
+
+    bb_ymax = etree.SubElement(bndbox, "ymax")
+    bb_ymax.text = str(ymax)
+
+    return obj
+
+
+def generate_pascal_template(imgname, username,
+                               width, height):
+
+    doc = etree.Element("annotation")
+
+    folder = etree.SubElement(doc, "folder")
+    folder.text = 'tiles'
+
+    filename = etree.SubElement(doc, "filename")
+    filename.text = imgname
+
+    # Create Source Tree
+    source = etree.SubElement(doc, "source")
+
+    database = etree.SubElement(source, "database")
+    database.text = 'HarpyFind'
+
+    annotation = etree.SubElement(source, "annotation")
+    annotation.text = 'PASCAL 2007'
+
+    image = etree.SubElement(source, "image")
+    image.text = 'flickr'
+
+    flickrid = etree.SubElement(source, "flickrid")
+    flickrid.text = 'N/A'
+
+    # Create Owner subtree
+    owner = etree.SubElement(doc, "owner")
+    owner_fid = etree.SubElement(owner, "flickrid")
+    owner_fid.text = 'N/A'
+    name = etree.SubElement(owner, "name")
+    name.text = 'N/A'
+
+    # Create Size Tree
+    size = etree.SubElement(doc, "size")
+    w = etree.SubElement(size, "width")
+    w.text = str(width)
+    h = etree.SubElement(size, "height")
+    h.text = str(height)
+
+    # Create Segmented Tree
+    segmented = etree.SubElement(doc, "segmented")
+    segmented.text = "0"
+
+    return doc
+
+def annotate_tiles(tagfile):
+    pascal_path = '../data/annotations/PASCAL/'
+    tiles_path = '../data/tiles/'
+
+    with open(tagfile, "r") as csvfile:
+        tags = csv.reader(csvfile, delimiter=',')
+
+        # skip the header
+        next(tags, None)
+
+        tileid = '0'
+
+        for row in tags:
+
+            if tileid != str(row[0]):
+
+                if tileid != '0':
+                    with open(pascal_path + tileid + ".xml", "w") as outfile:
+                        doc = etree.tostring(doc, pretty_print=True)
+                        outfile.write(doc.replace('  ', '\t'))
+
+                tileid = str(row[0])
+                print(tileid+".tif")
+                tif = gdal.Open(tiles_path+tileid+".tif")
+                tf = tif.GetGeoTransform()
+                channel = np.array(tif.GetRasterBand(1).ReadAsArray().astype(np.float32))
+
+                doc = generate_pascal_template(tileid, str(row[9]),
+                                               channel.shape[0],
+                                               channel.shape[1])
+
+            lat1 = float(row[5])
+            lon1 = float(row[6])
+            lat2 = float(row[7])
+            lon2 = float(row[8])
+            user = str(row[9])
+
+            xmin, ymin = gdal_utils.coord2pixel(tf, lat1, lon1)
+            xmax, ymax = gdal_utils.coord2pixel(tf, lat2, lon2)
+            doc.append(add_object(doc, xmin, ymin, xmax, ymax))
+
+# doc = generate_pascal_template("test", "matt", 100, 200)
+# print etree.tostring(doc, pretty_print=True).replace('  ', '\t')
+annotate_tiles('../data/tags/tiles_22feb17_0657.csv')
