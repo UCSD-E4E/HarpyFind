579b103bd0720af887c572b21003b1b3230b9f64
Matt Epperson <m.p.epperson@gmail.com>
2017-02-22 10:17:07 -0800

    TAGS_PARSER: added function for parsing tile csv

--- utils/tags_parser.py
+++ utils/tags_parser.py
@@ -3,6 +3,47 @@ import sys
 import csv
 import gdal_utils
 
+def tile_parser(filename, log="../data/tiles/log"):
+    tile_path = '../data/tiles'
+
+    with open(filename, "r") as csvfile:
+        tags = csv.reader(csvfile, delimiter=',')
+
+        # skip the header
+        next(tags, None)
+
+        # Make sure the directory for tiles
+        if not os.path.exists(tile_path):
+            os.makedirs(tile_path)
+
+        files = [file.strip('.tif') for file in os.listdir(tile_path)]
+
+        for row in tags:
+
+            if row[0] not in files:
+
+                tileID = int(row[0])
+
+                # HACK: Need to get bryan to change ordering
+                lat1 = float(row[4])
+                lon1 = float(row[1])
+                lat2 = float(row[2])
+                lon2 = float(row[3])
+
+                # lat1 = float(row[1])
+                # lon1 = float(row[2])
+                # lat2 = float(row[3])
+                # lon2 = float(row[4])
+
+                files.append(str(row[0]))
+
+                outfile = tile_path+'/'+row[0]+'.tif'
+
+                res = gdal_utils.trim(lat1, lon1, lat2, lon2,
+                                      '../data/bfree_ortho.tif', outfile)
+                print(row[0])
+                # print(res)
+
 
 def tag_parser(filename, log="../data/tiles/log"):
     report = []
@@ -16,10 +57,10 @@ def tag_parser(filename, log="../data/tiles/log"):
         for row in tags:
 
             # Make sure the directory for user exists
-            if not os.path.exists("../data/tiles/"+row[4]):
-                os.makedirs("../data/tiles/"+row[4])
+            if not os.path.exists("../data/trees/"+row[4]):
+                os.makedirs("../data/trees/"+row[4])
 
-            files = os.listdir("../data/tiles/"+row[4])
+            files = os.listdir("../data/trees/"+row[4])
 
             if row[6]+'.tif' not in files:
 
@@ -28,7 +69,7 @@ def tag_parser(filename, log="../data/tiles/log"):
                 lat2 = float(row[2])
                 lon2 = float(row[3])
 
-                outfile = '../data/tiles/'+row[4]+'/'+row[6]+'.tif'
+                outfile = '../data/trees/'+row[4]+'/'+row[6]+'.tif'
 
                 res = gdal_utils.trim(lat1, lon1, lat2, lon2,
                                       '../data/bfree_ortho.tif', outfile)
@@ -50,5 +91,7 @@ def tag_parser(filename, log="../data/tiles/log"):
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         tag_parser(sys.argv[1])
+        tile_parser(sys.argv[1])
     else:
-        tag_parser("../data/tags/tags_jan29_1137.csv")
+        # tag_parser("../data/tags/tags_jan29_1137.csv")
+        tile_parser("../data/tags/tiles_22feb17_0657.csv")
